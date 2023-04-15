adaadf8792b5e87b47ed8c9ef4d5c108076b87e8
EliChen00 <1251957442@qq.com>
2023-04-15 18:28:43 +0800

    create project

---
+++ src/main/java/com/example/recommend_backend/RecommendBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.recommend_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecommendBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/recommend_backend/RecommendBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recommend_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
